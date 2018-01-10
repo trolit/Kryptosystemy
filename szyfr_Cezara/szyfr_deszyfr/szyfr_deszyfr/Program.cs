801b8d69d97181ab88bea2983c662c0bb7078d13
Idziku <iddziku@gmail.com>
2018-01-10 18:38:52 +0100

    ...

---
+++ szyfr_Cezara/szyfr_deszyfr/szyfr_deszyfr/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szyfr_deszyfr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szyfr Cezara");
+            Console.WriteLine("Podaj słowo/zdanie do zaszyfrowania");
+            string slowo = Console.ReadLine();                    
+            char[] tablica = slowo.ToCharArray();           // dokonujemy konwersji do char
+            int rozmiar = tablica.Length;                   // w rozmiar zapisujemy długość tablicy
+            int i;
+
+            Console.WriteLine("\n\nZaszyfrowano: ");
+            for(i = 0; i < rozmiar; i++)                    // szyfrowanie i wypisanie zaszyfrowanego słowa
+            {
+                int znak = tablica[i]; 
+                znak = 65 + (znak - 62) % 26;
+                tablica[i] = (char)znak;
+                Console.Write(tablica[i]);
+            }
+
+            Console.WriteLine("\n\nOdszyfrowano: ");
+            for (i = 0; i < rozmiar; i++)                    // odszyfrowanie i wypisanie odszyfrowanego słowa
+            {
+                int znak = tablica[i];
+                znak = 65 + (znak - 42) % 26;
+                tablica[i] = (char)znak;
+                Console.Write(tablica[i]);
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}

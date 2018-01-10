2db76a79f081b773762959a7c40213e0a2251327
Idziku <iddziku@gmail.com>
2018-01-10 17:09:57 +0100

    Dodanie plikow

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Kryptosystemy
+## 10.01.18
+
+> 1. Szyfr Cezara(szyfr przesuwający):
+Jeden z najstarszych szyfrów. Jest zbudowany na zasadzie że, każdą literę tekstu niezaszyfrowanego zastępujemy inną przesuniętą
+względem litery kodowanej o stałą liczbę pozycji w alfabecie. Na przykład dla litery „a” (kod ASCII 97) przy przesunięciu o 2,
+literą kodowaną będzie „c” (kod ASCII 99) itd.... Nie gwarantuje on obecnie żadnego bezpieczeństwa. 
---
+++ szyfr_Cezara/dzialanie_szyfru/szyfr_c/szyfr_c/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szyfr_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Szyfr Cezara - pokaz");
+            Console.WriteLine("Aby wyjść wciśnij klawisz Escape");
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                if (keyInfo.KeyChar >= 97 && keyInfo.KeyChar <= 122)
+                {
+                    char znak = (char)(((keyInfo.KeyChar - 95) % 27) + 97);
+                    Console.Write(znak);
+                }
+            } while (keyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}

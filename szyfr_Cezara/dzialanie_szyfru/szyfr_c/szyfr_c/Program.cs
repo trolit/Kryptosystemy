5afbbde2dc3b22afcb6ccb18cd7ddc92ad27bd9d
Idziku <iddziku@gmail.com>
2018-01-10 17:26:08 +0100

    ...

--- szyfr_Cezara/dzialanie_szyfru/szyfr_c/szyfr_c/Program.cs
+++ szyfr_Cezara/dzialanie_szyfru/szyfr_c/szyfr_c/Program.cs
@@ -10,9 +10,11 @@ namespace szyfr_c
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Szyfr Cezara - pokaz");
+            Console.WriteLine("Szyfr Cezara - małe litery");
             Console.WriteLine("Aby wyjść wciśnij klawisz Escape");
             ConsoleKeyInfo keyInfo;
+            char[] tablica = new char[500];
+            int i = 0;
 
             do
             {
@@ -20,9 +22,14 @@ namespace szyfr_c
                 if (keyInfo.KeyChar >= 97 && keyInfo.KeyChar <= 122)
                 {
                     char znak = (char)(((keyInfo.KeyChar - 95) % 27) + 97);
+                    tablica[i] = znak;
+                    i++;
                     Console.Write(znak);
                 }
             } while (keyInfo.Key != ConsoleKey.Escape);
+
+            Console.WriteLine("koniec...");
+            Console.ReadKey();
         }
     }
 }

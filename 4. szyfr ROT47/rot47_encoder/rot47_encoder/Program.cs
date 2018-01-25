f31a62f60535f3c8a6bbe195214a127a0f42b4d1
Idziku <iddziku@gmail.com>
2018-01-25 19:46:26 +0100

    47 dziala

--- 4. szyfr ROT47/rot47_encoder/rot47_encoder/Program.cs
+++ 4. szyfr ROT47/rot47_encoder/rot47_encoder/Program.cs
@@ -10,6 +10,48 @@ namespace rot47_encoder
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Podaj tekst do zaszyfrowania przez algorytm ROT47");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetWindowSize(77, 20);
+            string tekst = Console.ReadLine();
+            int rozmiar = tekst.Length;
+            char[] tablica = new char[rozmiar];
+            tablica = tekst.ToCharArray();
+            int x;
+
+            for (x = 0; x < rozmiar; x++)                // SZYFROWANIE ROT47
+            {
+                char litera = tablica[x];
+                if ((int)litera >= 33 && (int)litera <= 126)    // jesli znak z przedzialu od 33 do 126
+                {
+                    int wartosc = 0;
+                    if ((int)litera <= 79)              // bo 80 + 47 daje 127 znak (a my mamy przedzial do 126 znakow)
+                    {
+                        wartosc = (int)litera + 47;
+                    }
+                    else
+                    {
+                        wartosc = ((int)litera + 47) - 94;    // przetestowane dla malej litery a bo 97+47-94 daje 50(czyli 2)
+                    }
+                    tablica[x] = (char)wartosc;
+                }
+                else
+                {
+                    tablica[x] = (char)litera;
+                }
+            }
+
+            Console.WriteLine("\nZaszyfrowany tekst przy pomocy ROT47:");
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (x = 0; x < rozmiar; x++)
+            {
+                Console.Write(tablica[x]);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n\nNaciśnij dowolny klawisz...");
+            Console.ReadKey();
         }
     }
 }

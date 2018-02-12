f82576225746fbb8c1bc84d3ec17cfcfc654c2f9
Idziku <iddziku@gmail.com>
2018-02-12 09:33:18 +0100

    enig

---
+++ 6. Enigma I/eni_one/eni_one/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eni_one
+{
+    class EnigmaCore
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENIGMA");
+            Console.WriteLine("- 3 wirniki");
+            Console.WriteLine("Podaj tekst do zaszyfrowania(tylko duże litery)");
+            string tekst = Console.ReadLine();
+            int rozmiar = tekst.Length;
+            char[] Chars_To_Encrypt = new char[rozmiar];
+            Chars_To_Encrypt = tekst.ToCharArray();
+        }
+    }
+}
